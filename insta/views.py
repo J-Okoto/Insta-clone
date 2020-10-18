bca4931123959d687365fcfddf00f6a03e5bef51
J-Okoto <okotojr@gmail.com>
2020-10-18 15:39:08 +0300

    added login/authentication system

--- insta/views.py
+++ insta/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
+@login_required(login_url='/accounts/login/')
 def profile(request):
     title = 'Profile'
     
