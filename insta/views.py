82adfaa828d7a3eef4e0144703254b534fa969ac
J-Okoto <okotojr@gmail.com>
2020-10-18 13:18:08 +0300

    added profile model

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def profile(request):
+    title = 'Profile'
+    
+    return render(request, 'profile/profile.html')
