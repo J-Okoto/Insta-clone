ac247908b09a2de8f52a9b691ee0c326da595123
J-Okoto <okotojr@gmail.com>
2020-10-18 15:48:23 +0300

    added user column in profile model

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Profile(models.Model):
     username = models.CharField(max_length=30,default='User')
     profile_photo = models.ImageField(upload_to="images/",null = True)
     bio = models.TextField(default='User does not have bio yet',blank=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null= True )
     
     def __str__(self):
         return self.username
