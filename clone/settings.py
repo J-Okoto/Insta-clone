850d51eddbd2630af099e1d6fc14ad70f2499268
J-Okoto <okotojr@gmail.com>
2020-10-21 00:27:00 +0300

    changes in settings

--- clone/settings.py
+++ clone/settings.py
@@ -132,12 +132,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
-    
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
